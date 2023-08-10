740e0a71e1a6764b87dbab4494586e41db842251
Luca Scionis <lucascionis@tiscali.it>
2023-08-10 10:21:53 +0200

    Update main.ino
    - add wifi strenght (in dbm) reading
    - trying to fix gps readings
    - removed network conf

--- board_src/main/main.ino
+++ board_src/main/main.ino
@@ -3,18 +3,20 @@
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
 
+/* Define this "net-conf.h" header with:
+      - SSID
+      - PASS
+      - MQTT_SERVER (name of the mqtt server)
+      - MQTT_CH_ID  (channel id)
+      - MQTT_PORT
+ */
+#include "net-conf.h"
+
 /* Sensors pins*/
 #define BPM_SENSOR  0
 #define GPS_RX      4
 #define GPS_TX      3
 #define GPS_BAUD    9600
-
-/* Net conf */
-#define SSID        "ssid"
-#define PASS        "pass"
-#define MQTT_SERVER "broker.mqtt.com"
-#define MQTT_CH_ID  0
-#define MQTT_PORT   1883
 
 /* Debug variables */
 #define DEBUG true  //set to true for debug output, false for no debug output
@@ -24,20 +26,22 @@
 
 
 /* Global variables */
-float bpm_read = 0.0;
+float bpm_read;
 float lat, lng;
+int   wifi_strength;
 
+/* Globa objects */
 WiFiClient nodeClient;
 PubSubClient mqttClient(nodeClient);
 
 TinyGPSPlus gps;
-SoftwareSerial gps_s(GPS_RX, GPS_TX);
+SoftwareSerial serialGPS(GPS_RX, GPS_TX);
 
 void setup() {
   DEBUG_SERIAL.begin(115200);
-  DEBUG_SERIAL.println();
+  serialGPS.begin(GPS_BAUD);
 
-  gps_s.begin(GPS_BAUD);
+  DEBUG_SERIAL.println();
   delay(3000);
 
   /* Setting WiFi connection */
@@ -54,10 +58,25 @@ void setup() {
   DEBUG_SERIAL.print("Connected, IP address: ");
   DEBUG_SERIAL.println(WiFi.localIP());
 
+  delay(2000);
+
   /* Setting MQTT server */
   #if MQTT_CONNECT == true
     mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
   #endif
+
+  delay(2000);
+}
+
+int getWifiQuality() {
+  if (WiFi.status() != WL_CONNECTED)
+    return -1;
+
+  int dBm = WiFi.RSSI();
+  if (dBm <= -100)
+    return 0;
+
+  return 2 * (dBm + 100);
 }
 
 void loop()
@@ -87,26 +106,35 @@ void loop()
     }
   #endif
   
-  // Retrieve sensors' readings
-  // bpm
+  /* Retrieving sensors' readings */
+
+  // BPM sensor (raw value)
   bpm_read = analogRead(BPM_SENSOR);
   // TODO: add a simple pre-processing to check if the reading is ok or not
   DEBUG_SERIAL.println(bpm_read);
 
-  while (gps_s.available() > 0)
+  // GPS coordinates reading
+  while (serialGPS.available() > 0)
   {
-    gps.encode(gps_s.read());
+    DEBUG_SERIAL.print("There is something for the gps!");
+    gps.encode(serialGPS.read());
+
+    if (gps.location.isValid())
+    {
+      lat = gps.location.lat();
+      lng = gps.location.lng();
+      DEBUG_SERIAL.print("Lat= ");
+      DEBUG_SERIAL.println(lat, 6);
+      DEBUG_SERIAL.print("Lng= ");
+      DEBUG_SERIAL.println(lng, 6);
+    }
   }
 
-  if (gps.location.isUpdated())
-  {
-    lat = gps.location.lat();
-    lng = gps.location.lng();
-    DEBUG_SERIAL.print("Lat= ");
-    DEBUG_SERIAL.println(lat, 6);
-    DEBUG_SERIAL.print("Lng= ");
-    DEBUG_SERIAL.println(lng, 6);
-  }
+  // WiFi strength (RSSI in dBm)  
+  // wifi_strength = getWifiQuality();
+  wifi_strength = WiFi.RSSI();
+  DEBUG_SERIAL.print("WiFi strength: ");
+  DEBUG_SERIAL.println(wifi_strength);
 
   delay(20);
 }
